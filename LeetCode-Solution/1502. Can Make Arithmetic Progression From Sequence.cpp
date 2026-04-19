ec1a31e139a0d1d54f081c9a5feaca5bed8de488
esraaehab333 <esraaehabb333@gmail.com>
2026-04-19 22:07:14 +0200

    solve: error in ide

---
+++ LeetCode-Solution/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        // -1 , 1 , 3 , 5 
+        int compare = arr[1] - arr[0]; // 1 --1 = 2 
+        for (int i = 0; i < arr.size() - 1; i++) {
+            if (arr[i + 1] - arr[i] != compare) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+int main() {
+
+    Solution sol;
+    vector<int> arr = { 1,3,8,9 };
+    cout << sol.canMakeArithmeticProgression(arr);
+    return 0;
+}
